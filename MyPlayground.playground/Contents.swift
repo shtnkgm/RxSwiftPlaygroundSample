88c6c240719a6e03a8c7e9d54f9b9dc54de5cf80
shtnkgm <nakagami.shota@gmail.com>
2018-09-25 19:28:26 +0900

    fix

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,61 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
 import RxSwift
+import Foundation
 
-var str = "Hello, playground"
+func log(_ message: String) {
+    print("---")
+    print("[" + message + "]")
+}
 
-print(str)
+let disposeBag = DisposeBag()
+let error = NSError(domain: "domain", code: 0, userInfo: nil)
+
+log("empty")
+Observable<Void>
+    .empty()
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+log("never")
+Observable<Void>
+    .never()
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+log("just")
+Observable<Int>
+    .just(1)
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+log("of")
+Observable<Int>
+    .of(1, 2, 3)
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+log("from")
+Observable<Int>
+    .from([1, 2, 3])
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+log("create")
+Observable<Int>.create { observer -> Disposable in
+    observer.onNext(1)
+    observer.onNext(2)
+    observer.onCompleted()
+    return Disposables.create()
+    }
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+log("error")
+
+Observable<Void>.error(error)
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+log("deffered")
+
